2b9d38c7b669052030e0446904a1e9f1d9f9e834
Savopg12 <savovujisic60@gmail.com>
2019-09-06 17:22:22 +0200

    Poboljsanje citljivosti koda

--- js/meals.js
+++ js/meals.js
@@ -1,64 +1,64 @@
 /**
  * Return one meal from themealdb.com in json.
  */
- function fetchMeal(idMeal) {
-     return fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+ idMeal)
-         .then(function(response) {
-             return response.json();
-         });
- }
+function fetchMeal(idMeal) {
+  return fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + idMeal)
+    .then(function(response) {
+      return response.json();
+    });
+}
 
 function fetchRandomMeal() {
-    return fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-        .then(function(response) {
-            return response.json();
-        });
+  return fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(function(response) {
+      return response.json();
+    });
 }
 
 function createMealCard(mealDate) {
-    return '<div class="card mb-3 col-sm-4">'
-        + '<div class="row no-gutters">'
-            + '<div class="col-md-4">'
-                + '<img src="' + mealDate.meals[0].strMealThumb + '" class="card-img" alt="...">'
-            + '</div>'
-        + '<div class="col-md-8">'
-            + '<div class="card-body">'
-                + '<h5 class="card-title">Title:' + mealDate.meals[0].strMeal + '</h5>'
-                + '<h5>Category:' + mealDate.meals[0].strCategory + '</h5>'
-                + '<h5>Tags:' + mealDate.meals[0].strTags + '</h5>'
-            + '</div>'
-          +'</div>'
-        + '</div>'
-      + '</div>'
+  return '<div class="card mb-3 col-sm-4">' +
+    '<div class="row no-gutters">' +
+    '<div class="col-md-4">' +
+    '<img src="' + mealDate.meals[0].strMealThumb + '" class="card-img" alt="...">' +
+    '</div>' +
+    '<div class="col-md-8">' +
+    '<div class="card-body">' +
+    '<h5 class="card-title">Title:' + mealDate.meals[0].strMeal + '</h5>' +
+    '<h5>Category:' + mealDate.meals[0].strCategory + '</h5>' +
+    '<h5>Tags:' + mealDate.meals[0].strTags + '</h5>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>'
 
 }
 
 function createMiniCard(mealDate) {
-  return '<a href="">'
-    + '<div class="card" style="width: 18rem;">'
-      +  '<img src="' + mealDate.meals[0].strMealThumb + '" class="card-img-top" alt="">'
-      +  '<div class="card-body">'
-      +    '<h3 class="card-text">' + mealDate.meals[0].strMeal + '</h3>'
-      +  '</div>'
-      + '</div>'
-     + '</a>';
+  return '<a href="">' +
+    '<div class="card" style="width: 18rem;">' +
+    '<img src="' + mealDate.meals[0].strMealThumb + '" class="card-img-top" alt="">' +
+    '<div class="card-body">' +
+    '<h3 class="card-text">' + mealDate.meals[0].strMeal + '</h3>' +
+    '</div>' +
+    '</div>' +
+    '</a>';
 }
 
 function bgCardSlider(mealDate) {
-  return '<a href="">'
-        + '<div class="card">'
-          +  '<img src="' + mealDate.meals[0].strMealThumb + '" class="card-img-top" alt="">'
-          +  '<div class="card-button">'
-          +    '<a data-id-meal="'  + mealDate.meals[0].idMeal + '" href="#" type="button" class="btn open-modal btn-warning">Read more</a>'
-          +  '</div>'
-        + '</div>'
-      + '</a>';
+  return '<a href="">' +
+    '<div class="card">' +
+    '<img src="' + mealDate.meals[0].strMealThumb + '" class="card-img-top" alt="">' +
+    '<div class="card-button">' +
+    '<a data-id-meal="' + mealDate.meals[0].idMeal + '" href="#" type="button" class="btn open-modal btn-warning">Read more</a>' +
+    '</div>' +
+    '</div>' +
+    '</a>';
 }
 
 function createSlider(slider, active) {
   var cards = [];
 
-  var resolveSlider = function () {
+  var resolveSlider = function() {
     var result = '<div class="carousel-item' + (active ? ' active' : '') + '"><div class="carousel-item-inner">';
     for (var i = 0; i < cards.length; ++i) {
       result = result + bgCardSlider(cards[i]);
@@ -68,24 +68,24 @@ function createSlider(slider, active) {
     initModalLinks();
   }
 
-  var promise = new Promise(function (resolve, reject) {
-      fetchRandomMeal().then(function (data) {
+  var promise = new Promise(function(resolve, reject) {
+    fetchRandomMeal().then(function(data) {
+      cards.push(data);
+      fetchRandomMeal().then(function(data) {
+        cards.push(data);
+        fetchRandomMeal().then(function(data) {
           cards.push(data);
-          fetchRandomMeal().then(function (data) {
-            cards.push(data);
-            fetchRandomMeal().then(function (data) {
-              cards.push(data);
-              resolveSlider();
-            });
-          });
+          resolveSlider();
+        });
       });
+    });
   });
 }
 
 function loadMore() {
   var topRated = document.querySelector('.top-rated');
   for (var i = 0; i < 3; ++i) {
-    fetchRandomMeal().then(function (data) {
+    fetchRandomMeal().then(function(data) {
       topRated.innerHTML = topRated.innerHTML + createMealCard(data);
       initModalLinks();
     });
@@ -93,7 +93,7 @@ function loadMore() {
 }
 
 function modalWindow() {
-  fetchMeal(this.dataset.idMeal).then(function (data) {
+  fetchMeal(this.dataset.idMeal).then(function(data) {
     $('#modal').modal({})
     var modalBody = document.querySelector('.modal-body')
     modalBody.innerHTML = createMealCard(data);
@@ -102,7 +102,7 @@ function modalWindow() {
 
 function initModalLinks() {
   var openModal = document.getElementsByClassName('open-modal')
-  for(var i = 0; i < openModal.length ; ++i) {
+  for (var i = 0; i < openModal.length; ++i) {
     openModal[i].addEventListener("click", modalWindow)
   }
 }
@@ -111,7 +111,7 @@ window.onload = function() {
   //
   var topRated = document.querySelector('.top-rated');
   for (var i = 0; i < 6; ++i) {
-    fetchRandomMeal().then(function (data) {
+    fetchRandomMeal().then(function(data) {
       topRated.innerHTML = topRated.innerHTML + createMealCard(data);
       initModalLinks();
     });
@@ -120,7 +120,7 @@ window.onload = function() {
   //
   var bestTeaser = document.querySelector('.teaser_best');
   for (var i = 0; i < 2; ++i) {
-    fetchRandomMeal().then(function (data) {
+    fetchRandomMeal().then(function(data) {
       bestTeaser.innerHTML = bestTeaser.innerHTML + createMiniCard(data);
       initModalLinks();
     });
@@ -128,12 +128,12 @@ window.onload = function() {
 
   var slider = document.querySelector('.carousel-inner');
   for (var i = 0; i < 3; ++i) {
-     createSlider(slider, i == 0 ? true : false);
-   }
+    createSlider(slider, i == 0 ? true : false);
+  }
 
 
-   var moreBtn = document.querySelector('.load-more-top-rated');
-   moreBtn.addEventListener("click", loadMore);
+  var moreBtn = document.querySelector('.load-more-top-rated');
+  moreBtn.addEventListener("click", loadMore);
 
 
 }
